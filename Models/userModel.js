84ae64e1c84de4df72f747995ec933510cab3648
tingamapuro04 <alponcemobutu100@gmail.com>
2023-09-09 21:15:58 +0300

    hash password before save

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -14,3 +15,30 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 })
+
+userSchema.pre('save', async function() {
+  try {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+  } catch (error) {
+    console.error(error.message);
+  }
+})
+
+
+userSchema.statics.login = async function (email, password) {
+  
+  const user = await User.findOne({ email });
+  if(!user){
+    throw Error("User not found");
+  }
+
+  const comparePass = await bcrypt.compare(password, user.password);
+  if (!comparePass) {
+    throw Error("Wrong credentials");
+  }
+  return user;
+}
+
+export const User = mongoose.model("User", userSchema);
+
