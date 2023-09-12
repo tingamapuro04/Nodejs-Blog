42924ed6ab5ca8d8500c4c01f8745520c473c97f
tingamapuro04 <alponcemobutu100@gmail.com>
2023-09-12 08:57:18 +0300

    category controllers

--- Controllers/categoryController.js
+++ Controllers/categoryController.js
@@ -14,3 +14,71 @@ export const addCategory = async (req, res) => {
     })
   }
 }
+
+export const getCategories = async (req, res) => {
+  
+  try {
+    const cats = await Category.find({});
+    res
+      .status(201)
+      .json({ message: "Successful get request", data: cats });
+  } catch (error) {
+    res.status(500).json({
+      mess: "Failure",
+      data: error.message,
+    });
+  }
+};
+
+export const getCategory = async (req, res) => {
+  const { cat_id } = req.params;
+  try {
+    const cat = await Category.findById(cat_id)
+    res.status(201).json({ message: "Successful get request for single category", data: cat });
+  } catch (error) {
+    res.status(500).json({
+      mess: "Failure",
+      data: error.message,
+    });
+  }
+};
+
+
+export const updateCategory = async (req, res) => {
+  const { cat_id } = req.params;
+  const { body } = req;
+  try {
+    const cat = await Category.findByIdAndUpdate(id, body);
+    res
+      .status(201)
+      .json({
+        message: "Successful update",
+        data: cat,
+      });
+  } catch (error) {
+    res.status(500).json({
+      mess: "Failure",
+      data: error.message,
+    });
+  }
+};
+
+
+export const deleteCategory = async (req, res) => {
+  const { cat_id } = req.params;
+  try {
+    const cat = await Category.findByIdAndDelete(cat_id);
+    res
+      .status(201)
+      .json({
+        message: "Category delete successfully",
+        data: cat,
+      });
+  } catch (error) {
+    res.status(500).json({
+      mess: "Failure",
+      data: error.message,
+    });
+  }
+};
+
