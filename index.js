069359908c4ad49dd338733e17dc58c68a2ac2e0
tingamapuro04 <alponcemobutu100@gmail.com>
2023-09-14 20:15:54 +0300

    cors options to allow clint side

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import multer from 'multer';
+import cors from "cors";
 import { errorHandler } from "./Middlewares/serverErrorHandler.js";
 import { router } from "./Route/routes.js";
 const app = express();
 dotenv.config();
 
+// cors options
+const corsOptions = {
+  origin: "http://localhost:5173", // Replace with your frontend's domain
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+
 // Middlewares
 app.use(express.json());
 app.use('/api/v1/', router);
+app.use(cors(corsOptions));
 
 
 const { DATABASE_URL, PORT } = process.env;
@@ -23,6 +35,22 @@ mongoose
   .catch((error) => {
     console.error(error);
   });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb)=>{
+    cb(null,'./images')
+  },
+    filename:(req,file,cb)=>{
+      cb(null, req.body.name)
+    }
+});
+
+const upload = multer({storage: storage});
+app.post('/api/uploads', upload.single('file'), (req, res) => {
+  res.status(200).json({mess: "File uploaded successfully"});
+})
+
+
 
 
 
